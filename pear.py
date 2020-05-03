539ec8023f819cdfa3b85780e8fe7e3619605020
Zhang Yuanrui <55218898+Dosrui78@users.noreply.github.com>
2020-05-03 19:11:54 +0800

    Add files via upload
    爬取梨视频首页视频并下载

---
+++ pear.py
@@ -0,0 +1,126 @@
+import requests
+import logging
+import os
+import time
+import random
+from parsel import Selector
+import re
+from requests import ReadTimeout, HTTPError, ConnectionError
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def User_Agent():
+    User_Agent = [
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+        "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+        "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+        "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+        "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+        "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+        "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+        "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+        "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10"
+    ]
+    this_ua = random.choice(User_Agent)
+    global header
+    header = {"User-Agent": this_ua,
+              'Referer': r'{BASE_URL}/category_2'}
+
+BASE_URL = 'https://www.pearvideo.com'
+def scrape_url(url):
+    logging.info('scraping %s...', url)
+    try:
+        res = requests.get(url, headers=header)
+        return res.text
+    except TimeoutError as a:
+        logging.info(f'Error time is Out:{a}')
+    except ReadTimeout as b:
+        logging.info(f'Error ReadTimeout:{b}')
+    except ConnectionError as c:
+        logging.info(f'Error ConnectionError:{c}')
+    except HTTPError as d:
+        logging.info(f'Error URLError:{d}')
+
+
+def scrape_index(url):
+    #logging.info('scraping index %s', url)
+    return scrape_url(url)
+
+
+def parse_index(content):
+    sel = Selector(content)
+    lists = sel.css('.act-main.cmmain')
+    #all_tags = []
+    for list in lists:
+        big_url = list.css('div.vervideo-blist-big > div.vervideo-bd > a::attr("href")').getall()
+        #yield big_url
+        small_url1 = list.css('div.vervideo-blist-small > ul > li > div > a::attr("href")').getall()
+        #print(small_url1)
+        small_url2 = list.css('div.vervideo-blist-small.pcshow > ul > li > div > a::attr("href")').getall()
+        #print(small_url2)
+        all_tags = big_url + small_url1 +small_url2
+        print(len(all_tags))
+        return all_tags
+
+def scrape_detail(tag):
+    detail_url = f'{BASE_URL}/{tag}'
+    logging.info('getting detail %s', detail_url)
+    return scrape_url(detail_url)
+
+def parse_detail(html):
+    sel = Selector(html)
+    global title
+    title = sel.css('#detailsbd > div.details-main.vertical-details.cmmain > div:nth-child(4) > div > div.box-left.clear-mar > h1::text').get()
+    logging.info('The Video Title: %s', title)
+    pattern = re.compile('srcUrl="(.*?.mp4)"')
+    video_url = re.findall(pattern, html)[0]
+    logging.info('The Video link:%s', video_url)
+    content = requests.get(video_url, headers=header).content
+    path = 'PEAR_VIDEO'
+    if not os.path.exists(path):
+        os.mkdir(path)
+    name = video_url.split('/')[-1]
+    file_path = path + '/' + name
+    with open(file_path, 'wb') as f:
+        f.write(content)
+        logging.info(f"{title}Download Successfully!")
+
+def main():
+    User_Agent()
+    url = 'https://www.pearvideo.com/'
+    html = scrape_index(url)
+    all_tags = parse_index(html)
+    for tag in all_tags:
+        detail_html = scrape_detail(tag)
+        parse_detail(detail_html)
+
+
+if __name__ == '__main__':
+    main()
+    time.sleep(3)
